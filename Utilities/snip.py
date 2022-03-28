8743aa05f705eff12d05eb518c7fc8e07e8a0b90
Iyyanar Madeshvaran - Linux <inr1137m@gmail.com>
2022-03-28 15:32:15 +0530

    fixes for full page screenshot

--- Utilities/snip.py
+++ Utilities/snip.py
@@ -6,8 +6,8 @@ from selenium import webdriver
 # from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.chrome.options import Options
 
-# screen_width = 1024
-# screen_height = 768
+screen_width = 1024
+screen_height = 768
 # FFdriver = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),'drivers','geckodriver') #Options()
 CHMdriver = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),'drivers','chromedriver') #Options()
 options = Options()
@@ -22,22 +22,23 @@ def takeSnip(url):
             return "Empty Url", 400
         # driver = webdriver.Firefox(options=options, executable_path=FFdriver)
         driver = webdriver.Chrome(options=options, executable_path=CHMdriver)
-        driver.maximize_window()
+        # driver.maximize_window()  
         driver.get(url)
-        s = driver.get_window_size()
-        w = driver.execute_script('return document.body.parentNode.scrollWidth')
-        h = driver.execute_script('return document.body.parentNode.scrollHeight')
-        driver.set_window_size(w, h)
-        sleep(1)
-        imgBin = driver.find_element_by_tag_name('body').screenshot_as_png()
-        driver.set_window_size(s['width'], s['height'])
+        # s = driver.get_window_size()
+        # w = driver.execute_script('return document.body.parentNode.scrollWidth')
+        # h = driver.execute_script('return document.body.parentNode.scrollHeight')
+        # driver.set_window_size(w, h)
+        # sleep(1)
+        e1 = driver.find_element_by_tag_name('body')
+        imgBin = e1.screenshot_as_png()
+        # driver.set_window_size(s['width'], s['height'])
         driver.quit()
         
         # print("base64 : {}".format(b64))
         # outfile = os.path.join(output_directory, filename)
         # # driver.get_screenshot_as_file(outfile)
         # # driver.get_full_page_screenshot_as_file(outfile)
-        driver.quit()
+        # driver.quit()
         return imgBin, 200
     except Exception as e:
         return "Exception : {}".format(e), 400
