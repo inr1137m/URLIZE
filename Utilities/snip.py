84bd4126f287a2d97620b553abdd786d129787da
Iyyanar Madeshvaran - Linux <inr1137m@gmail.com>
2022-03-28 14:36:22 +0530

    updated with chromedriver

--- Utilities/snip.py
+++ Utilities/snip.py
@@ -1,16 +1,18 @@
+from calendar import WEDNESDAY
 import os
 from datetime import datetime
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+# from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 
 screen_width = 1024
 screen_height = 768
 # FFdriver = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),'drivers','geckodriver') #Options()
-FFdriver = os.path.join(os.path.dirname(os.path.realpath(__file__)),'geckodriver') #Options()
-print("FF DRIVER loc : {}".format(FFdriver))
+CHMdriver = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),'drivers','chromedriver') #Options()
 options = Options()
 options.add_argument("--headless")
+# options.add_argument("window-size=1400,1500")
 
 def takeSnip(url):
     try:
@@ -18,7 +20,8 @@ def takeSnip(url):
         print("get " + url + "...")
         if url == 'empty':
             return "Empty Url", 400
-        driver = webdriver.Firefox(options=options, executable_path=FFdriver)
+        # driver = webdriver.Firefox(options=options, executable_path=FFdriver)
+        driver = webdriver.Chrome(options=options, executable_path=CHMdriver)
         driver.set_window_size(screen_width, screen_height)
         driver.get(url)
         sleep(3)
